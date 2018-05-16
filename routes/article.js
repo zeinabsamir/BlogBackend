e1cd2c069bbd13443f3bc0ae2275b9e7d561a2a4
zeinab <zeinabsamir8@gmail.com>
2018-05-16 04:33:52 +0200

    fix some error

--- routes/article.js
+++ routes/article.js
@@ -3,17 +3,18 @@ const router = express.Router();
 const db = require('../models');
 
 router.get('/', (req, res, next) => {
-  db.Post.findAll().then((posts) => {
-    res.json(posts);
+  db.Article.findAll().then((articles) => {
+    res.json(articles);
   });
 });
 
 router.post('/', (req, res, next) => {
-  const post = req.body;
+  const article = req.body;
 
   // Check sequelize docs
-  db.Post.create(post).then(createdPost => {
-    res.json(createdPost);
+  db.Article.create(article).then(createdArticle => {
+    res.json(createdArticle);
   });
 });
 
+module.exports = router;
